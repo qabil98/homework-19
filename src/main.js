9a29a916cb42e2be5c173477b8948b81056f50a9
qabil98 <qedimaliyev1998@gmail.com>
2024-05-07 10:28:39 +0400

    completed

---
+++ src/main.js
@@ -0,0 +1,20 @@
+let ad;
+let soyad;
+let yas;
+
+ad = prompt("adinizi daxil edin");
+soyad=prompt("soyadinizi daxil edin");
+yas = prompt("yasinizi daxil edin");
+
+if(yas<18){
+    alert("qeydiyatdan kecmek ucun yasiniz azdir")
+}
+else if(yas>18){
+    alert("qeydiyyat ucun yasiniz coxdur");
+}
+else if(yas=18){
+    alert("qeydiyyat ugurla tamamlandi");
+}
+else{
+    
+}
